8df89a7b89827b6eefb3c5f758cb9d44cc564864
JustinRPeter <justinrpeter@gmail.com>
2015-06-21 15:43:59 +1000

    Plot one. First commit.

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#Coursera Exploratory data analysis. Project 2.
+
+#Justin Peter. 21 June 2015.
+
+#---Clean up the workspace.
+    rm(list=ls())
+
+#---Read the data sets.
+    NEI <- readRDS("summarySCC_PM25.rds")
+    SCC <- readRDS("Source_Classification_Code.rds")
+
+#---Evaluate the total emissions by year
+    yr.tots <- aggregate(Emissions ~ year, data=NEI,sum)
+
+#---Plot using barplot. More intuitive than a line plot
+
+    png("plot1.png")
+    barplot(yr.tots$Emissions/1.e06,names.arg=yr.tots$year,
+            xlab="Year",
+            ylab=expression("Total PM"[2.5]*" emissions (millions of tons)"),
+            main=expression("Total PM"[2.5]*" emissions from all sources"))
+    dev.off()
+
